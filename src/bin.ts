a689a95018ae73d19d710c7841547b49aabe1436
Taylan Pince <taylanpince@gmail.com>
2024-07-04 10:51:28 +0200

    Basic CLI structure

--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
 
-console.log("Hello, world!");
+import { Command } from 'commander';
+import figlet from "figlet"
+
+console.log(figlet.textSync("Sequence"))
+console.log("")
+
+const program = new Command()
+
+program.action(() => {
+	program.help()
+})
+
+function makeCommandMarketplace() {
+	const opt = new Command('marketplace');
+
+	opt.action(() => {
+		opt.help();
+	})
+
+	opt.command('create-listings').action(() => {
+		console.log('create listings');
+	})
+
+	return opt;
+}
+program.addCommand(makeCommandMarketplace())
+
+program.parse(process.argv)
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsup";
  
 export default defineConfig({
-  entry: ["src/bin.ts"],
+  entry: [
+    "src/bin.ts"
+  ],
   publicDir: false,
   clean: true,
   minify: true,

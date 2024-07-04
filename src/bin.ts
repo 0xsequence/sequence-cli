6c983c9b3816e1ed85245ffc8eb71d90e046bc76
Taylan Pince <taylanpince@gmail.com>
2024-07-04 14:08:34 +0200

    Add initial version of create listing command

--- src/bin.ts
+++ src/bin.ts
@@ -2,41 +2,72 @@
 
 import { Command } from '@commander-js/extra-typings'
 import { input, password, number } from '@inquirer/prompts'
+import { ethers } from 'ethers'
+import { sequence } from '0xsequence';
+import { Session } from '@0xsequence/auth'
+import { findSupportedNetwork } from '@0xsequence/network'
 
 import figlet from "figlet"
+
+const SEQUENCE_MARKETPLACE_V1_ADDRESS = "0xB537a160472183f2150d42EB1c3DD6684A55f74c"
+const ORDERBOOK_ABI = [
+    {
+        inputs: [
+          {
+            components: [
+              { internalType: 'bool', name: 'isListing', type: 'bool' },
+              { internalType: 'bool', name: 'isERC1155', type: 'bool' },
+              { internalType: 'address', name: 'tokenContract', type: 'address' },
+              { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+              { internalType: 'uint256', name: 'quantity', type: 'uint256' },
+              { internalType: 'uint96', name: 'expiry', type: 'uint96' },
+              { internalType: 'address', name: 'currency', type: 'address' },
+              { internalType: 'uint256', name: 'pricePerToken', type: 'uint256' },
+            ],
+            internalType: 'struct ISequenceMarketStorage.RequestParams',
+            name: 'request',
+            type: 'tuple',
+          },
+        ],
+        name: 'createRequest',
+        outputs: [{ internalType: 'uint256', name: 'requestId', type: 'uint256' }],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    }
+]
 
 console.log(figlet.textSync("Sequence"))
 console.log("")
 
 const program = new Command()
 
-program.version('0.1.0', '-v, --vers', 'Display the current version').action(() => {
+program.version('0.1.0', '-v, --version', 'Display the current version').action(() => {
     program.help()
 })
 
 async function createListings(options: any) {
-    let private_key = options.key
-    let collection_address = options.address
-    let token_id = options.token
+    let privateKey = options.key
+    let collectionAddress = options.address
+    let tokenId = options.token
     let network = options.network
     let currency = options.currency
     let price = options.price
     let quantity = options.quantity
 
-    if (!private_key) {
-        private_key = await password({
+    if (!privateKey) {
+        privateKey = await password({
             message: 'Enter the private key for the wallet that holds the tokens'
         })
     }
 
-    if (!collection_address) {
-        collection_address = await input({
+    if (!collectionAddress) {
+        collectionAddress = await input({
             message: 'Enter the address of the collection contract'
         })
     }
 
-    if (!token_id) {
-        token_id = await number({
+    if (!tokenId) {
+        tokenId = await number({
             message: 'Enter the ID for the token to be listed'
         })
     }
@@ -54,12 +85,64 @@ async function createListings(options: any) {
     }
 
     if (!price) {
-        price = await number({
-            message: 'Enter the price per token'
+        price = await input({
+            message: 'Enter the price per token, in the currency specified'
         })
     }
 
-    console.log('create-listings', private_key, collection_address, token_id, network, currency, price, quantity)
+    const chainConfig = findSupportedNetwork(network);
+
+    if (chainConfig === undefined) {
+        program.error("Unsupported network, please select a valid network")
+        return
+    }
+
+    let nodeUrl = chainConfig.rpcUrl
+
+    if (options.projectAccessKey) {
+        nodeUrl += "/" + options.key
+    }
+    console.log(`Using network: ${network} (${chainConfig.chainId})`)
+    const provider = new ethers.providers.JsonRpcProvider(nodeUrl)
+    const wallet = new ethers.Wallet(privateKey, provider)
+    const session = await Session.singleSigner({
+        signer: wallet,
+        projectAccessKey: options.projectAccessKey,
+    })
+
+    const sequenceMarketplaceInterface = new ethers.utils.Interface(ORDERBOOK_ABI)
+
+    const dataCreateRequest = sequenceMarketplaceInterface.encodeFunctionData(
+        "createRequest",
+        [{
+            isListing: true,
+            isERC1155: true,
+            tokenContract: ethers.utils.hexlify(collectionAddress),
+            tokenId: BigInt(tokenId),
+            quantity: BigInt(quantity),
+            expiry: BigInt(Date.now() + (options.expireIn * 24 * 60 * 60)),
+            currency: ethers.utils.hexlify(currency),
+            pricePerToken: ethers.utils.parseUnits(price, 18).toBigInt(),
+        }]
+    )
+
+    const txn = {
+        to: SEQUENCE_MARKETPLACE_V1_ADDRESS,
+        data: dataCreateRequest
+    }
+
+    const signer = session.account.getSigner(chainConfig.chainId);
+
+    try {
+        const res = await signer.sendTransaction(txn)
+        console.log(`Transaction sent: ${res.hash}`)
+
+        const receipt = await provider.getTransactionReceipt(res.hash)
+        console.log(`Transaction receipt: ${receipt.transactionHash}`)
+    } catch (error) {
+        console.log(error)
+        program.error('Error processing transaction, please try again.')
+    }
 }
 
 function makeCommandMarketplace() {
@@ -76,8 +159,10 @@ function makeCommandMarketplace() {
     .option('-t, --token <token_id>', 'ID for the token to be listed', parseInt)
     .option('-n, --network <network>', 'Network to be used (mainnet, polygon, etc.)')
     .option('-c, --currency <currency>', 'Either a well known currency code (USDC, MATIC, etc.) or a token address')
-    .option('-p, --price <price>', 'Price per token', parseFloat)
+    .option('-p, --price <price>', 'Price per token, in the currency specified')
     .option('-q, --quantity <quantity>', 'Number of tokens to be listed', parseInt, 1)
+    .option('--expire-in <expiry_time>', 'Days to expiry for the listing', parseInt, 7)
+    .option('--project-access-key <access_key>', 'Project access key for Sequence requests')
     .action(options => {
         createListings(options)
     })

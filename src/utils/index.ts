45cac9a6e1b18f9e0e48e5026cb9377a7c1806a4
Richard Irala <richardirala47@hotmail.com>
2024-09-20 03:34:26 -0300

    Create reusable prompt component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { Wallet } from 'ethers'
+import { select } from '@inquirer/prompts';
 
 export function isValidPrivateKey(privateKey: string) {
     try {
@@ -7,4 +8,21 @@ export function isValidPrivateKey(privateKey: string) {
     } catch (error) {
         return false;
     }
+}
+
+export async function promptUserKeyCustomizationDecision () : Promise<boolean> {
+    return await select({
+        message:
+          "Would you like to use test configuration keys for testing, or customize your configuration keys?",
+        choices: [
+          {
+            name: "Use test keys",
+            value: false,
+          },
+          {
+            name: "I'll provide my own keys",
+            value: true,
+          },
+        ],
+      });
 }

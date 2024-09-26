7c5d8e74d6ac88faf7703e209e26001eb02e52f6
moskalyk <morgan.moskalyk@protonmail.ch>
2024-09-26 13:50:56 -0400

    removed spaces

--- src/boilerplates/boilerplates.ts
+++ src/boilerplates/boilerplates.ts
@@ -17,28 +17,28 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-embedded-wallet-react-starter")
         .description("Clone a starter boilerplate for Sequence Embedded Wallet integrated with React")
         .option(
-            "--waas-config-key <waas_key>",
-            "WaaS config key for this project"
+        "--waas-config-key <waas_key>",
+        "WaaS config key for this project"
         )
         .option(
-            "--project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "--project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--google-client-id <google_client_id>",
-            "Google client ID to be used during authentication"
+        "--google-client-id <google_client_id>",
+        "Google client ID to be used during authentication"
         )
         .option(
-            "--apple-client-id <apple_client_id>",
-            "Apple client ID to be used during authentication"
+        "--apple-client-id <apple_client_id>",
+        "Apple client ID to be used during authentication"
         )
         .option(
-            "--wallet-connect-id <wallet_connect_id>",
-            "Wallet Connect ID to be used during authentication"
+        "--wallet-connect-id <wallet_connect_id>",
+        "Wallet Connect ID to be used during authentication"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createEmbeddedWalletReact(program, options);
@@ -48,16 +48,16 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-server-side-transactions")
         .description("Create a server that has the ability to mint collectibles based on parameters")
         .option(
-            "-k, --key <private_key>",
-            "Private key for the wallet that holds the tokens"
+        "-k, --key <private_key>",
+        "Private key for the wallet that holds the tokens"
         )
         .option(
-            "-pak, --project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "-pak, --project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createServerSideTx(program, options);
@@ -67,28 +67,28 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-kit-embedded-wallet-nextjs-starter")
         .description("Clone a starter boilerplate for Sequence Embedded Wallet integrated with Sequence Kit and Next.js")
         .option(
-            "--waas-config-key <waas_key>",
-            "WaaS config key for this project"
+        "--waas-config-key <waas_key>",
+        "WaaS config key for this project"
         )
         .option(
-            "--project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "--project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--google-client-id <google_client_id>",
-            "Google client ID to be used during authentication"
+        "--google-client-id <google_client_id>",
+        "Google client ID to be used during authentication"
         )
         .option(
-            "--apple-client-id <apple_client_id>",
-            "Apple client ID to be used during authentication"
+        "--apple-client-id <apple_client_id>",
+        "Apple client ID to be used during authentication"
         )
         .option(
-            "--wallet-connect-id <wallet_connect_id>",
-            "Wallet Connect ID to be used during authentication"
+        "--wallet-connect-id <wallet_connect_id>",
+        "Wallet Connect ID to be used during authentication"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createEmbeddedWalletNextjs(program, options);
@@ -98,24 +98,24 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-embedded-wallet-linking-starter")
         .description("Clone a starter boilerplate for Sequence Embedded wallet linking demo integrated with React")
         .option(
-            "--waas-config-key <waas_key>",
-            "WaaS config key for this project"
+        "--waas-config-key <waas_key>",
+        "WaaS config key for this project"
         )
         .option(
-            "--project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "--project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--google-client-id <google_client_id>",
-            "Google client ID to be used during authentication"
+        "--google-client-id <google_client_id>",
+        "Google client ID to be used during authentication"
         )
         .option(
-            "--wallet-connect-id <wallet_connect_id>",
-            "Wallet Connect ID to be used during authentication"
+        "--wallet-connect-id <wallet_connect_id>",
+        "Wallet Connect ID to be used during authentication"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createWalletLinkingEmbeddedWallet(program, options);
@@ -125,20 +125,20 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-embedded-wallet-verify-session-starter")
         .description("Clone a starter boilerplate for Sequence Embedded Wallet verification from a server-side application")
         .option(
-            "--waas-config-key <waas_key>",
-            "WaaS config key for this project"
+        "--waas-config-key <waas_key>",
+        "WaaS config key for this project"
         )
         .option(
-            "--project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "--project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--google-client-id <google_client_id>",
-            "Google client ID to be used during authentication"
+        "--google-client-id <google_client_id>",
+        "Google client ID to be used during authentication"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createEmbeddedWalletVerifySession(program, options);
@@ -148,28 +148,28 @@ export function makeCommandBoilerplates(program: Command) {
         .command("create-primary-drop-sale-starter")
         .description("Clone a starter boilerplate for Primary Drop Sale integrated with WaaS")
         .option(
-            "--waas-config-key <waas_key>",
-            "WaaS config key for this project"
+        "--waas-config-key <waas_key>",
+        "WaaS config key for this project"
         )
         .option(
-            "--project-access-key <access_key>",
-            "Project access key for Sequence requests"
+        "--project-access-key <access_key>",
+        "Project access key for Sequence requests"
         )
         .option(
-            "--google-client-id <google_client_id>",
-            "Google client ID to be used during authentication"
+        "--google-client-id <google_client_id>",
+        "Google client ID to be used during authentication"
         )
         .option(
-            "--apple-client-id <apple_client_id>",
-            "Apple client ID to be used during authentication"
+        "--apple-client-id <apple_client_id>",
+        "Apple client ID to be used during authentication"
         )
         .option(
-            "--wallet-connect-id <wallet_connect_id>",
-            "Wallet Connect ID to be used during authentication"
+        "--wallet-connect-id <wallet_connect_id>",
+        "Wallet Connect ID to be used during authentication"
         )
         .option(
-            "--verbose",
-            "Show additional information in the output"
+        "--verbose",
+        "Show additional information in the output"
         )
         .action((options) => {
             createPrimaryDropSale(program, options);

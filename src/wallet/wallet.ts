8a399b39f9a06be93157e27c763e5c31f5e601f4
Taylan Pince <taylanpince@gmail.com>
2024-10-17 13:48:08 +0200

    Identify sequence wallet from txn (#19)
    * Released 0.3.1
    
    * Add method for identifying Sequence wallets from a txn
    
    * README fix

--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import { createSingleSigner } from "./create_single_signer";
+import { identifySequenceWallet } from "./identify_sequence_wallet";
 
 
 export function makeCommandWallet(program: Command) {
@@ -30,6 +31,29 @@ export function makeCommandWallet(program: Command) {
         )
         .action((options) => {
             createSingleSigner(program, options);
+        });
+
+    comm
+        .command("identify-sequence-wallet")
+        .description("Identify Sequence Wallet address from a transaction hash")
+        .option(
+            "-t, --txn <txn>",
+            "Transaction hash to be used"
+        )
+        .option(
+            "-n, --network <network>",
+            "Network to be used (mainnet, polygon, etc.)"
+        )
+        .option(
+            "--project-access-key <access_key>",
+            "Project access key for Sequence requests"
+        )
+        .option(
+            "--verbose",
+            "Show additional information in the output"
+        )
+        .action((options) => {
+            identifySequenceWallet(program, options);
         });
 
     return comm;

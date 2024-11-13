a470ed5bf318d4b565f75e69c1c3b8bd803ff263
Lukas Jenicek <lj@horizon.io>
2024-11-13 17:18:59 +0100

    add semicolons, remove wrong rules in prettier

--- src/marketplace/create_listings.ts
+++ src/marketplace/create_listings.ts
@@ -1,12 +1,12 @@
-import { Command } from 'commander'
-import { input, password, number } from '@inquirer/prompts'
-import { ethers, Numeric } from 'ethers'
-import { findSupportedNetwork } from '@0xsequence/network'
-import { isValidPrivateKey } from '../utils/'
-import { ERC1155_ABI } from '../abi/ERC_1155'
-import { Orderbook_ABI } from '../abi/Orderbook'
-import { ERC721_ABI } from '../abi/ERC_721'
-import { polygon } from 'viem/chains'
+import { Command } from 'commander';
+import { input, password, number } from '@inquirer/prompts';
+import { ethers, Numeric } from 'ethers';
+import { findSupportedNetwork } from '@0xsequence/network';
+import { isValidPrivateKey } from '../utils/';
+import { ERC1155_ABI } from '../abi/ERC_1155';
+import { Orderbook_ABI } from '../abi/Orderbook';
+import { ERC721_ABI } from '../abi/ERC_721';
+import { polygon } from 'viem/chains';
 import {
   createWalletClient,
   http,
@@ -14,122 +14,122 @@ import {
   getContract,
   WalletClient,
   Hex,
-} from 'viem'
-import { privateKeyToAccount } from 'viem/accounts'
+} from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 
 const SEQUENCE_MARKETPLACE_V1_ADDRESS =
-  '0xB537a160472183f2150d42EB1c3DD6684A55f74c'
+  '0xB537a160472183f2150d42EB1c3DD6684A55f74c';
 
 type ListingRequest = {
-  collectionAddress: Address
-  walletAddress: Address
+  collectionAddress: Address;
+  walletAddress: Address;
   // currency address or well-known currency (USDC, MATIC)
-  currency: Address | string
-  tokenId: bigint
-  price: bigint
-  quantity: Numeric
-  expiry: Numeric
-}
+  currency: Address | string;
+  tokenId: bigint;
+  price: bigint;
+  quantity: Numeric;
+  expiry: Numeric;
+};
 
 export async function createListings(program: Command, options: any) {
-  let privateKey = options.key
-  let collectionAddress = options.address
-  let tokenId = options.token
-  let network = options.network
-  let currency = options.currency
-  let price = options.price
-  let quantity = options.quantity
-  const isERC1155 = options.type === 'ERC1155'
+  let privateKey = options.key;
+  let collectionAddress = options.address;
+  let tokenId = options.token;
+  let network = options.network;
+  let currency = options.currency;
+  let price = options.price;
+  let quantity = options.quantity;
+  const isERC1155 = options.type === 'ERC1155';
 
   if (!isERC1155) {
     // ERC721 must be always quantity = 1
-    quantity = 1
+    quantity = 1;
   }
 
   if (!privateKey) {
     privateKey = await password({
       message: 'Enter the private key for the wallet that holds the tokens',
-    })
+    });
 
     if (!isValidPrivateKey(privateKey) && privateKey) {
-      console.log('Please input a valid EVM Private key')
-      process.exit()
+      console.log('Please input a valid EVM Private key');
+      process.exit();
     }
   }
 
   if (!collectionAddress) {
     collectionAddress = await input({
       message: 'Enter the address of the collection contract',
-    })
+    });
   }
 
   if (tokenId === null || tokenId === undefined) {
     tokenId = await number({
       message: 'Enter the ID for the token to be listed',
-    })
+    });
   }
 
   if (!network) {
     console.log(
       'Please provide the Network for your project as a Sequence chain handle'
-    )
+    );
     console.log(
       'Possible networks can be found at https://docs.sequence.xyz/solutions/technical-references/chain-support'
-    )
+    );
 
     network = await input({
       message: 'Enter the network to be used (mainnet, polygon, etc.)',
-    })
+    });
   }
 
   if (!currency) {
     currency = await input({
       message:
         'Enter either a well known currency code (USDC, MATIC, etc.) or a token address',
-    })
+    });
   }
 
   if (!price) {
     price = await input({
       message: 'Enter the price per token, in the currency specified',
-    })
+    });
   }
 
-  const chainConfig = findSupportedNetwork(network)
+  const chainConfig = findSupportedNetwork(network);
   if (chainConfig === undefined) {
-    program.error('Unsupported network, please select a valid network')
-    return
+    program.error('Unsupported network, please select a valid network');
+    return;
   }
 
-  let nodeUrl = chainConfig.rpcUrl
+  let nodeUrl = chainConfig.rpcUrl;
 
   if (options.projectAccessKey) {
-    nodeUrl += '/' + options.projectAccessKey
+    nodeUrl += '/' + options.projectAccessKey;
   }
 
   if (options.verbose) {
-    console.log(`Using node URL: ${nodeUrl}`)
+    console.log(`Using node URL: ${nodeUrl}`);
   }
 
-  const provider = new ethers.JsonRpcProvider(nodeUrl)
-  const wallet = new ethers.Wallet(privateKey, provider)
+  const provider = new ethers.JsonRpcProvider(nodeUrl);
+  const wallet = new ethers.Wallet(privateKey, provider);
 
   // Set up the wallet client
   const walletClient = createWalletClient({
     account: privateKeyToAccount(privateKey),
     chain: polygon,
     transport: http(nodeUrl), // Replace with your Polygon node URL
-  })
+  });
 
-  let walletAddress = wallet.address as Address
+  let walletAddress = wallet.address as Address;
   if (!walletAddress.startsWith('0x')) {
-    walletAddress = `0x${walletAddress}`
+    walletAddress = `0x${walletAddress}`;
   }
 
-  console.log(`Using EOA Wallet: ${walletAddress}`)
-  console.log('Collection Address:', collectionAddress)
-  console.log('Currency token address:', currency)
-  console.log('Contract Type: %s, Is 1155', options.type, isERC1155)
+  console.log(`Using EOA Wallet: ${walletAddress}`);
+  console.log('Collection Address:', collectionAddress);
+  console.log('Currency token address:', currency);
+  console.log('Contract Type: %s, Is 1155', options.type, isERC1155);
 
   const listingRequest = {
     collectionAddress,
@@ -139,21 +139,21 @@ export async function createListings(program: Command, options: any) {
     price,
     quantity,
     expiry: options.expireIn,
-  }
+  };
 
   if (isERC1155) {
     try {
-      await approvalERC1155(walletClient, listingRequest)
+      await approvalERC1155(walletClient, listingRequest);
     } catch (e) {
-      console.dir(e, { depth: null })
-      return
+      console.dir(e, { depth: null });
+      return;
     }
   } else {
     try {
-      await approvalERC721(walletClient, listingRequest)
+      await approvalERC721(walletClient, listingRequest);
     } catch (e) {
-      console.dir(e, { depth: null })
-      return
+      console.dir(e, { depth: null });
+      return;
     }
   }
 
@@ -161,13 +161,13 @@ export async function createListings(program: Command, options: any) {
     address: SEQUENCE_MARKETPLACE_V1_ADDRESS,
     abi: Orderbook_ABI,
     client: walletClient,
-  })
+  });
 
   try {
-    const currentTimeInSeconds = Math.floor(Date.now() / 1000)
-    const expiryInSeconds = listingRequest.expiry * 24 * 60 * 60
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+    const expiryInSeconds = listingRequest.expiry * 24 * 60 * 60;
 
-    console.log('Creating listing')
+    console.log('Creating listing');
 
     const res = await marketplaceContract.write.createRequest(
       [
@@ -186,15 +186,15 @@ export async function createListings(program: Command, options: any) {
         account: walletClient.account,
         chain: walletClient.chain,
       }
-    )
+    );
 
-    console.log('Listing created', res)
+    console.log('Listing created', res);
   } catch (error) {
-    console.dir(error, { depth: null })
-    program.error('Error processing transaction, please try again.')
+    console.dir(error, { depth: null });
+    program.error('Error processing transaction, please try again.');
   }
 
-  console.log(`Listing created successfully for token ID ${tokenId}`)
+  console.log(`Listing created successfully for token ID ${tokenId}`);
 }
 
 async function approvalERC1155(client: WalletClient, input: ListingRequest) {
@@ -202,14 +202,14 @@ async function approvalERC1155(client: WalletClient, input: ListingRequest) {
     address: input.collectionAddress as Hex,
     abi: ERC1155_ABI,
     client: client,
-  })
+  });
 
   const isApprovedForAll = await collectionContract.read.isApprovedForAll(
     [input.walletAddress, SEQUENCE_MARKETPLACE_V1_ADDRESS],
     {
       account: client.account,
     }
-  )
+  );
 
   if (!isApprovedForAll) {
     await collectionContract.write.setApprovalForAll(
@@ -218,8 +218,8 @@ async function approvalERC1155(client: WalletClient, input: ListingRequest) {
         account: client.account as any,
         chain: client.chain,
       }
-    )
-    console.log('ERC1155 setApprovalForAll executed successfully')
+    );
+    console.log('ERC1155 setApprovalForAll executed successfully');
   }
 }
 
@@ -228,20 +228,20 @@ async function approvalERC721(client: WalletClient, input: ListingRequest) {
     address: input.collectionAddress as Hex,
     abi: ERC721_ABI,
     client: client,
-  })
+  });
 
   const isApprovedForAll = await collectionContract.read.isApprovedForAll(
     [input.walletAddress, SEQUENCE_MARKETPLACE_V1_ADDRESS],
     {
       account: client.account,
     }
-  )
+  );
 
   if (!isApprovedForAll) {
     await collectionContract.write.setApprovalForAll(
       [SEQUENCE_MARKETPLACE_V1_ADDRESS, true],
       { account: client.account as any, chain: client.chain }
-    )
-    console.log('ERC721 setApprovalForAll executed successfully')
+    );
+    console.log('ERC721 setApprovalForAll executed successfully');
   }
 }

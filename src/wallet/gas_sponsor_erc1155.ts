682da96357acd7346ae18c9611cfa7f46367ecdb
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2025-01-29 11:30:37 +0300

    Add gas sponsor test command

---
+++ src/wallet/gas_sponsor_erc1155.ts
@@ -0,0 +1,84 @@
+import { Command } from 'commander';
+import { password, input } from '@inquirer/prompts';
+import { ethers, FunctionFragment } from 'ethers';
+import { findSupportedNetwork } from '@0xsequence/network';
+import { isValidPrivateKey } from '../utils/';
+import { ERC1155_ABI } from '../abi/ERC_1155';
+import { Session } from '@0xsequence/auth';
+
+export async function testGasSponsorERC1155(program: Command, options: any) {
+  let privateKey = options.key;
+  let contractAddress = options.contract;
+  let network = options.network;
+  let projectAccessKey = options.projectAccessKey;
+
+  // Get required inputs
+  if (!privateKey) {
+    privateKey = await input({
+      message: 'Enter private key for the wallet initiating transaction',
+    });
+
+    if (!isValidPrivateKey(privateKey)) {
+      program.error('Invalid private key format');
+      return;
+    }
+  }
+
+  if (!projectAccessKey) {
+    projectAccessKey = await input({
+      message: 'Enter your Sequence project access key',
+    });
+  }
+
+  if (!network) {
+    network = await input({
+      message: 'Enter network (e.g. polygon, mainnet)',
+    });
+  }
+
+  const chainConfig = findSupportedNetwork(network);
+
+  if (chainConfig === undefined) {
+    program.error('Unsupported network, please select a valid network');
+  }
+
+  const session = await Session.singleSigner({
+    signer: privateKey,
+    projectAccessKey: options.projectAccessKey,
+  });
+
+  const signer = session.account.getSigner(chainConfig.chainId, {
+    selectFee: async (_txs: any, options: any[]) => {
+      const found = options.find(o => !o.token.contractAddress);
+
+      return found;
+    },
+  });
+
+  // Get contract address
+  if (!contractAddress) {
+    contractAddress = await input({
+      message: 'Enter ERC1155 contract address',
+    });
+  }
+
+  // Create contract instance
+  const contract = new ethers.Contract(contractAddress, ERC1155_ABI, signer);
+
+  try {
+    // Execute gas-sponsored transfer
+    const _operator = session.account.address;
+    const _approved = 'true';
+
+    console.log('Executing gas-sponsored transfer...');
+    const txn = await contract.setApprovalForAll.populateTransaction(
+      _operator,
+      _approved
+    );
+    const txnResponse = await signer.sendTransaction(txn);
+    const txnReceipt = await txnResponse.wait();
+    console.log(txnReceipt?.hash);
+  } catch (error: any) {
+    program.error(`Transaction failed: ${error}`);
+  }
+}

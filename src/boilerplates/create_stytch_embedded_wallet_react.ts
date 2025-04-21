8ad6588576891a42be0eda7609378f23f31914d9
Richard Irala <richardirala47@hotmail.com>
2025-04-21 18:37:17 -0300

    Improve logs and add repo clone check in create-stytch-embedded-wallet-react-starter command

--- src/boilerplates/create_stytch_embedded_wallet_react.ts
+++ src/boilerplates/create_stytch_embedded_wallet_react.ts
@@ -1,16 +1,20 @@
 
 import { Command } from "commander";
-import { promptForGoogleClientIdWithLogs, promptForProjectAccessKeyWithLogs, promptForWaaSConfigKeyWithLogs, promptForStytchWithLogs, promptUserKeyCustomizationDecision, writeDefaultKeysToEnvFileIfMissing, writeToEnvFile } from "../utils";
+import { promptForProjectAccessKeyWithLogs, promptForWaaSConfigKeyWithLogs, promptForStytchWithLogs, promptUserKeyCustomizationDecision, writeDefaultKeysToEnvFileIfMissing, writeToEnvFile, cliConsole, checkIfDirectoryExists } from "../utils";
 import { EnvKeys } from "../utils/types";
 
 import shell from "shelljs";
 
 const STYTCH_EMBEDDED_WALLET_REACT_REPO_URL = "https://github.com/0xsequence-demos/stytch-embedded-wallet-react-boilerplate/";
+const REPOSITORY_FILENAME = "stytch-embedded-wallet-react-boilerplate";
+const REPOSITORY_REFERENCE = "Stytch Embedded Wallet React boilerplate";
 
 export async function createStytchEmbeddedWalletReact(program: Command, options: any) {
     let waasConfigKey = options.waasConfigKey;
     let projectAccessKey = options.projectAccessKey;
     let stytchPublicToken = options.stytchPublicToken;
+
+    cliConsole.sectionTitle(`Initializing creation process for ${REPOSITORY_REFERENCE} 🚀`);
 
     const userWantsToConfigureTheirKeys = false
     
@@ -20,16 +24,22 @@ export async function createStytchEmbeddedWalletReact(program: Command, options:
         stytchPublicToken = await promptForStytchWithLogs(stytchPublicToken);
     }
 
-    console.log("Cloning the repo to `stytch-embedded-wallet-react-boilerplate`...");
+    cliConsole.loading(`Cloning the repo to '${REPOSITORY_FILENAME}'`);
 
-    shell.exec(`git clone ${STYTCH_EMBEDDED_WALLET_REACT_REPO_URL} stytch-embedded-wallet-react-boilerplate`, { silent: !options.verbose });
+    shell.exec(`git clone ${STYTCH_EMBEDDED_WALLET_REACT_REPO_URL} ${REPOSITORY_FILENAME}`, { silent: !options.verbose });
     
-    shell.cd("stytch-embedded-wallet-react-boilerplate");
+    const directoryExists = checkIfDirectoryExists(REPOSITORY_FILENAME);
+        
+    if (!directoryExists) {
+        cliConsole.error("Repository cloning failed. Please try again.");
+        return;
+    }
 
+    shell.cd(REPOSITORY_FILENAME);
     
     shell.exec(`touch .env`, { silent: !options.verbose });
 
-    console.log("Configuring your project...");
+    cliConsole.loading("Configuring your project");
 
     const envExampleContent = shell.cat('.env.example').toString();
     const envExampleLines = envExampleContent.split('\n');
@@ -43,12 +53,12 @@ export async function createStytchEmbeddedWalletReact(program: Command, options:
     writeToEnvFile(envKeys, options);
     writeDefaultKeysToEnvFileIfMissing(envExampleLines, envKeys, options);
 
-    console.log("Installing dependencies...");
+    cliConsole.loading("Installing dependencies");
     
     shell.exec(`pnpm install`, { silent: !options.verbose });
 
-    console.log("Stytch Embedded Wallet React boilerplate created successfully! 🚀");
-    console.log("Starting development server...");
+    cliConsole.done(`${REPOSITORY_REFERENCE} created successfully! 🚀`);
+    cliConsole.loading("Starting development server");
 
     shell.exec(`pnpm dev`, { silent: false });
 }

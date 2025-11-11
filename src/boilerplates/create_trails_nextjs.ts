396eed69868329628deff43acf01b25873d2a7ed
Richard Miguel Irala <richardirala47@hotmail.com>
2025-11-11 16:36:50 -0300

    Update trails commands to include Trails API key .env variable

--- src/boilerplates/create_trails_nextjs.ts
+++ src/boilerplates/create_trails_nextjs.ts
@@ -1,7 +1,14 @@
 import { Command } from 'commander';
-import { checkIfDirectoryExists, cliConsole } from '../utils';
+import {
+  checkIfDirectoryExists,
+  cliConsole,
+  promptForTrailsApiKeyWithLogs,
+  writeDefaultKeysToEnvFileIfMissing,
+  writeToEnvFile,
+} from '../utils';
 
 import shell from 'shelljs';
+import { EnvKeys } from '../utils/types';
 
 const TRAILS_REPO_URL =
   'https://github.com/0xsequence-demos/trails-nextjs-starter/';
@@ -12,9 +19,23 @@ export async function createTrailsNextjsStarter(
   program: Command,
   options: any
 ) {
+  let trailsApiKey = options.trailsApiKey;
+
   cliConsole.sectionTitle(
     `Initializing creation process for ${REPOSITORY_REFERENCE} 🚀`
   );
+
+  const userWantsToConfigureTheirKeys = true;
+
+  if (userWantsToConfigureTheirKeys) {
+    trailsApiKey = await promptForTrailsApiKeyWithLogs(trailsApiKey);
+    if (!trailsApiKey) {
+      cliConsole.info('You left the Trails API key empty in your .env file.');
+      cliConsole.info(
+        'Remember to go back and add your API key later so the project works properly.'
+      );
+    }
+  }
 
   cliConsole.loading(`Cloning the repo to '${REPOSITORY_FILENAME}'`);
 
@@ -29,6 +50,20 @@ export async function createTrailsNextjsStarter(
   }
 
   shell.cd(REPOSITORY_FILENAME);
+
+  shell.exec(`touch .env`, { silent: !options.verbose });
+
+  cliConsole.loading('Configuring your project');
+
+  const envExampleContent = shell.cat('.env.example').toString();
+  const envExampleLines = envExampleContent.split('\n');
+
+  const envKeys: EnvKeys = {
+    NEXT_PUBLIC_TRAILS_API_KEY: trailsApiKey || undefined,
+  };
+
+  writeToEnvFile(envKeys, options);
+  writeDefaultKeysToEnvFileIfMissing(envExampleLines, envKeys, options);
 
   cliConsole.loading('Installing dependencies');
 
